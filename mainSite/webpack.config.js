ebb18aef6d024b8493a2db1db10545e284bb085f
Tristan Elliott <>
2021-09-03 21:48:09 -0300

    adding babel to project

--- mainSite/webpack.config.js
+++ mainSite/webpack.config.js
@@ -1,8 +1,30 @@
-var path = require('path');
+const path = require('path');
+
+let mode = "development";
+if(process.env.NODE_ENV === "production"){
+  mode = "production";
+}
 
 module.exports = {
+  mode: mode,
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        use:{
+          loader:'babel-loader',
+          options:{
+            presets:['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  
+  devtool:"source-map",
   devServer: {
-   
+    contentBase:"./dist",
     compress: true,
     port: 9000,
   },
