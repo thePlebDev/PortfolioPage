063b3d90aa83c57f3cfd6c163024f6f729c948a8
Tristan Elliott <>
2021-09-04 17:51:05 -0300

    adding link css

--- mainSite/src/components/Title.js
+++ mainSite/src/components/Title.js
@@ -5,8 +5,15 @@ import React from 'react'
 const Title =()=>{
 
     return(
-        <div class="container">
+        <div>
+        <div class="container" >
             <h1 class="title">Elliott Software</h1>
+        </div>
+        <div  class="subcontainer">
+            <button class="hover-text">About</button>
+            <button class="hover-text">Projects</button>
+            <button class="hover-text">Contact</button>
+        </div>
         </div>
     )
 }
